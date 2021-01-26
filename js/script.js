5c2c61c3e6af3f8bb450b3993760eceb57cc1a6a
juljul98 <mateojulius98@yahoo.com>
2021-01-26 23:37:58 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100 ) {
+            $('.header-top').addClass('fixed');
+        } else {
+            $('.header-top').removeClass('fixed');
+        }
+    });
+
+
+    $('.hambuger-menu a').click(function(e){
+        e.preventDefault();
+        var $this = $(this),
+            href = $this.attr('href');
+        $this.parent().toggleClass('active');
+        $(href).slideToggle('active');
+        if($this.parent().hasClass('active')) {
+            $('body').append('<div class="overlay"></div>');
+        } else {
+            $('.overlay').remove(); 
+        }
+    });
+
+    $('.slick-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll:3,
+        dots: false,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                prevArrow: false,
+                nextArrow: false
+              }
+            },
+        ]
+    });
+});
